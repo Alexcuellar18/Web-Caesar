3888a51331225ee35b2cddc7505a766da37d50cf
Alex Cuellar <alejandrocuellar18@gmail.com>
2019-09-16 14:21:47 -0400

    added code to render answer on same textarea box

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ form = """<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,8 +31,7 @@ form = """<!DOCTYPE html>
         Rotate by:
           <input type='text' name='rot' default=0/>
         </label>
-          <textarea name='text'>
-          </textarea>
+          <textarea name='text'>{0}</textarea>
           <input type='submit' value='Rotate Me'/>
       </form>
     </body>
@@ -40,7 +39,7 @@ form = """<!DOCTYPE html>
 
 @app.route("/")
 def index():
-  return form
+  return form.format("")
 
 @app.route('/', methods=['POST'])
 def encrypt():
@@ -49,7 +48,7 @@ def encrypt():
 
   rotated = rotate_string(text,rot)
 
-  return "<h1>" + rotated + "</h1>"
+  return form.format(rotated)
 
 
 app.run()
